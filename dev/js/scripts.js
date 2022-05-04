0fa61bdf8af9de8e7d5101287a3aa2b167dfe668
HopeZucco <hmz34@drexel.edu>
2022-05-03 20:25:41 -0400

    Commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,10 @@
-// import { gsap } from "gsap";
+ import { gsap } from "gsap";
+ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+
+    gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin)
+  
+
+  const maintTL = gsap.timeline();
+  
+maintTL
